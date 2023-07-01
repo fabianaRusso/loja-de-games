281afe973bbd33ac00b46a362b4b728350a88ec4
fabianaRusso <fabi.pereiraa@hotmail.com>
2023-07-01 12:22:33 -0300

    Projeto loja de games

---
+++ src/main/java/com/generation/loja_de_games/model/Categoria.java
@@ -0,0 +1,75 @@
+package com.generation.loja_de_games.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo tipo é obrigatório")
+	@Size(min = 3, max = 100, message = "O atributo tipo deve conter no mínimo 03 e no máximo 100 caracteres")
+	private String tipo;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
+
+	
+}
---
+++ src/main/java/com/generation/loja_de_games/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.loja_de_games.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.loja_de_games.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findByTipoContainingIgnoreCase(@Param("tipo") String tipo);
+
+}
